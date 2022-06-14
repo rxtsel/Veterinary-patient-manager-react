0561b8916ab96c0e82efb09057f5c8c189d9c190
Cristhian Melo <rxtsel@hotmail.com>
2022-06-14 15:13:37 -0500

    chore: remove githubcorner

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,24 @@ import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Formulario from './components/Formulario'
 import ListadoPacientes from './components/ListadoPacientes'
-import GithubCorner from 'react-github-corner';
-import Footer from "./components/Footer";
+import Footer from './components/Footer'
 
-function App() {
-
-  const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes')) ?? [])
+function App () {
+  const [pacientes, setPacientes] = useState(JSON.parse(window.localStorage.getItem('pacientes')) ?? [])
   const [paciente, setPaciente] = useState({})
-  
-  // useEffect( () => {
-  //   const getLocalStorage = () => {
-  //     const pacientesLocalStorage = JSON.parse(localStorage.getItem('pacientes')) ?? []
-  //     setPacientes(pacientesLocalStorage)
-  //   }
-  //   getLocalStorage()
-  // }, [] )
 
-  useEffect( () => {
-    localStorage.setItem('pacientes', JSON.stringify( pacientes ))
-  }, [pacientes] )
-  
+  useEffect(() => {
+    window.localStorage.setItem('pacientes', JSON.stringify(pacientes))
+  }, [pacientes])
+
   const eliminarPaciente = id => {
-    const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id )
+    const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id)
     setPacientes(pacientesActualizados)
   }
 
   return (
-    <div className="container mx-auto mt-20">
+    <div className='container mx-auto mt-20'>
       <Header />
-      <GithubCorner href="https://github.com/rxtsel/Veterinary-patient-manager-react" target="_blank" size={100}/>
       <div className='mt-12 mx-12 md:flex'>
         <Formulario
           pacientes={pacientes}
@@ -44,7 +33,7 @@ function App() {
           eliminarPaciente={eliminarPaciente}
         />
       </div>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
+import Theme from './Theme'
+
 const Header = () => {
   return (
     <>
-      <h1 className="font-black text-5xl mx-auto text-center md:w-2/3">Seguimiento Pacientes <span className="text-indigo-600">Veterinaria</span></h1>
+      <h1 className='font-black text-5xl mx-auto text-center md:w-2/3'>Seguimiento Pacientes <span className='text-indigo-600'>Veterinaria</span></h1>
+      <Theme />
     </>
   )
 }
